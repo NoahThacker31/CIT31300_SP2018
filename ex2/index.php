e70a7ffb05ce54e6dcf673aac72d29e76cd67300
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2018-02-04 20:27:53 -0500

    Add files via upload

---
+++ ex2/index.php
@@ -0,0 +1,36 @@
+<?php
+include 'classes/user.class.php';
+include 'classes/registered_user.class.php';
+include 'classes/admin_user.class.php';
+?>
+
+<head></head>
+
+<body>
+<?php
+	$regUser = new registered_user("Regular User", "crdillon");
+	$adminUser = new admin_user("Administrator", "dldillon");
+	
+	//$userLevel = "Regular User";
+	//$userId = "crdillon";
+	
+	//$userLevel = "Administrator";
+	//$userId = "dldillon";
+
+	echo "User Level: " . $regUser->get_user_level() . "<br>";
+	echo "Registered User ID: " . $regUser->get_user_id() . "<br>";
+	echo "Registered User Type: " . $regUser->get_user_type() . "<br>";
+	echo "Registered First Name: " . $regUser->get_fname() . "<br>";
+	echo "Registered Last Name: " . $regUser->get_lname() . "<br>";
+	echo "Registered Email: " . $regUser->get_email() . "<br>" . "<br>";
+	
+	echo "User Level: " . $adminUser->get_user_level() . "<br>";
+	echo "Admin User ID: " . $adminUser->get_user_id() . "<br>";
+	echo "Admin User Type: " . $adminUser->get_user_type() . "<br>";
+	echo "Admin First Name: " . $adminUser->get_fname() . "<br>";
+	echo "Admin Last Name: " . $adminUser->get_lname() . "<br>";
+	echo "Admin Email: " . $adminUser->get_email();
+?>
+</body>
+
+<footer></footer>
