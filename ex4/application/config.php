ba287b37df1fceba9272afa88f0acb677d57a65c
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2018-03-11 23:27:04 -0400

    Add files via upload

---
+++ ex4/application/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:22041/CIT313/SP2018/ex4/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'nthacker');
+define('DB_PASS', 'Then it is an even fight2');
+define('DB_NAME', 'sp18_31300_nthacker');
