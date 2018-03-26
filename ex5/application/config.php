944c93547a66e33a7660112ffaacf8ddc4f57fa7
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2018-03-25 23:05:57 -0400

    Exercise 5
    Logout, Protected Pages, User Friendly Login

---
+++ ex5/application/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:22041/CIT313/SP2018/ex5/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'nthacker');
+define('DB_PASS', '31gabbie');
+define('DB_NAME', 'nthacker_db');
