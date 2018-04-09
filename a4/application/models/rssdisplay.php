3111c61fe663c73361dabf7dbf8f1a8ddaa2ec42
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2018-04-08 21:55:22 -0400

    Assignment 4
    RSS, API, Ajax, Weather, News (Home)

---
+++ a4/application/models/rssdisplay.php
@@ -0,0 +1,34 @@
+<?php
+class RssDisplay extends Model{
+	protected $feed_url;
+	protected $num_feed_items;
+	protected $channel_info;
+	public $x;
+	
+	public function __construct($url){	
+		parent::__construct();
+		$this->feed_url = $url;
+	}
+	
+	public function getFeedItems($num_feed_items){
+		$this->num_feed_items = $num_feed_items;
+        $rss = simplexml_load_file($this->feed_url);
+        $i = 0;
+        $rss_array = array();
+        if($this->num_feed_items > 0) {
+            foreach($rss->channel->item as $feed_data) {
+                $rss_array[] = $feed_data;
+                if($i > $num_feed_items ) {
+                    break;
+                }
+                $i++;
+            }
+           return $rss_array;
+        }
+	}
+	
+	public function getChannelInfo(){
+		$rss = simplexml_load_file($this->feed_url);
+        return ($rss->channel);
+	}
+}
